38e5d5a5046b314f7c02897599d776a06ed0fdbe
Seto <sethjespinas@gmail.com>
2022-10-14 08:29:29 +0800

    Routing initial setup

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php require __DIR__ . '/vendor/autoload.php';
+<?php
+require __DIR__ . '/vendor/autoload.php';
 
 use App\Lib\App;
+use App\Lib\Router;
+use App\Lib\Request;
+use App\Lib\Response;
+
+Router::get('/', function () {
+    echo 'This is the Home Page';
+});
+
+Router::get('/book/([0-9]*)', function (Request $req, Response $res) {
+    $res->toJSON([
+        'book' =>  ['id' => $req->params[0]],
+        'status' => 'ok'
+    ]);
+});
 
 App::run();
